e4a80ea9bb0d9ab5815c255a9daf5f991515080a
HuangHaoce <huang_haoce@163.com>
2018-06-08 22:39:27 +0800

    add

--- wxapp/src/pages/index/index.vue
+++ wxapp/src/pages/index/index.vue
@@ -9,20 +9,25 @@ export default {
   data () {
     return {
       dailyUrl: '',
-      height: 3420
+      height: 0
     }
   },
   components: {
     today
   },
   onLoad: function (options) {
+    // 域名备案中，用本地接口开发
+    // https://www.clayhuang.cn/everyday
     var that = this
+    const screenWidth = wx.getSystemInfoSync().windowWidth
     wx.request({
-      url: 'https://www.clayhuang.cn/everyday/api/daily/today',
+      url: 'http://127.0.0.1:3030/api/daily/today',
       success: function (res) {
         const data = res.data
+        console.log(data)
         if (data.result) {
           that.dailyUrl = data.url
+          that.height = (screenWidth * data.shape.height) / data.shape.width
         } else {
           console.log('error')
         }
